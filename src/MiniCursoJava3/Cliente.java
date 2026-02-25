f0780c291873ef9563706f0712ef000a5a5c1be8
luisaa32 <162749134+luisaa32@users.noreply.github.com>
2026-02-25 18:05:57 -0300

    Update Cliente.java

--- src/MiniCursoJava3/Cliente.java
+++ src/MiniCursoJava3/Cliente.java
@@ -5,14 +5,12 @@ public class Cliente {
     private int idade;
     private boolean nomeSujo;
 
-    // Construtor que o seu print utiliza
     public Cliente(String nome, int idade, boolean nomeSujo) {
         this.nome = nome;
         this.idade = idade;
         this.nomeSujo = nomeSujo;
     }
 
-    // Getters e Setters
     public String getNome() { return nome; }
     public int getIdade() { return idade; }
     public boolean isNomeSujo() { return nomeSujo; }
